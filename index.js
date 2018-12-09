26f7ff015b96468a6aa3c77dc9e44f858e107645
Jared <jcamstan@gmail.com>
2018-12-09 14:45:28 -0700

    Update New Routes
    Fawn integration, movies rentals genres updates

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+const Joi = require('joi')
+Joi.objectId = require('joi-objectid')(Joi);
+
+
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const rentals = require('./routes/rentals');
+const movies = require('./routes/movies');
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB'))
@@ -12,6 +18,8 @@ mongoose.connect('mongodb://localhost/vidly')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/rentals', rentals);
+app.use('/api/movies', movies);
 
 app.get('/', (req, res) => {
     res.send('Welcome to Vidly');
--- models/genre.js
+++ models/genre.js
@@ -35,5 +35,6 @@ function validateGenre(genre){
   return Joi.validate(genre, schema);
 }
 
+exports.genreSchema = genreSchema
 exports.Genre = Genre;
 exports.validate = validateGenre;
