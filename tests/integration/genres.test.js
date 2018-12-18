0883a395cf71cca5044177aa40713ca19b785a5a
Jared <jcamstan@gmail.com>
2018-12-18 13:17:49 -0700

    Finish Up testing

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -8,8 +8,8 @@ let server;
 describe('/api/genres', () => {
   beforeEach(() => { server = require('../../index'); })
   afterEach(async () => { 
-    server.close(); 
     await Genre.remove({});
+    await server.close(); 
   });
 
   describe('GET /', () => {
